9b4e22a759c11f179c8377f0209126aba6fa8c43
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-01-10 18:10:09 +0700

    create navbar on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom'
-import Navigationbar from './components/Navigationbar';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Navigationbar from "./components/Navigationbar";
 
 function App() {
   return (
     <>
       <Navigationbar />
       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
-        <Route path='/detail' element={<h1>Detail</h1>} />
-        <Route path='/cart' element={<h1>Cart</h1>} />
-        <Route path='/profile' element={<h1>Profile</h1>} />
-        <Route path='/dashboard' element={<h1>Dashboard</h1>} />
-        <Route path='/add-product' element={<h1>Add Product</h1>} />
-        <Route path='/list-product' element={<h1>List Product</h1>} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/detail" element={<h1>Detail</h1>} />
+        <Route path="/cart" element={<h1>Cart</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+        <Route path="/add-product" element={<h1>Add Product</h1>} />
+        <Route path="/list-product" element={<h1>List Product</h1>} />
       </Routes>
     </>
   );
--- client/src/components/Navigationbar.js
+++ client/src/components/Navigationbar.js
@@ -1,5 +1,71 @@
-const Navigationbar = () => {
-    return <h1>Ini navbar, navbar itu diatas</h1>
-}
+import { Navbar, Container, Nav, Button, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default Navigationbar
+const Navigationbar = () => {
+  return (
+    <Navbar
+      bg="light"
+      variant="light"
+      className={`position-absolute top-0 w-100`}
+      style={{
+        backgroundColor: "#F5F5F5",
+        height: 100,
+        zIndex: 2,
+        boxShadow: "0px 0px 20px #888888",
+      }}
+    >
+      <Container style={{ zIndex: 3 }}>
+        <Link to="/" className={`navbar-brand`}>
+          <Image
+            src="/assets/NavbarIcon.svg"
+            alt="WaysBeans"
+            style={{ width: 250 }}
+          />
+        </Link>
+        <Nav className="ms-auto">
+          <Button
+            variant="outline-light"
+            className="d-block d-lg-none"
+            // onClick={() => {
+            //   setLoginForm(true);
+            // }}
+          >
+            Login
+          </Button>
+          <Button
+            id="navLoginButton"
+            className="mx-2 d-none d-lg-block fw-5 hoveredButton"
+            style={{
+              width: 150,
+              border: "2px solid #613D2B",
+              color: "#613D2B",
+              backgroundColor: "#F5F5F5",
+              fontWeight: "bold",
+            }}
+            // onClick={() => {
+            //   setLoginForm(true);
+            // }}
+          >
+            Login
+          </Button>
+          <Button
+            className="text-white mx-2 d-none d-lg-block hoveredButton"
+            style={{
+              width: 150,
+              backgroundColor: "#613D2B",
+              border: "2px solid #613D2B",
+              fontWeight: "bold",
+            }}
+            // onClick={() => {
+            //   setRegisterForm(true);
+            // }}
+          >
+            Register
+          </Button>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigationbar;
