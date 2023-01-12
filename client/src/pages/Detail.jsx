85897ad0880fb8f990e016779f7e0a770e1a2635
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-01-12 23:33:14 +0700

    fetching detail product data

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,6 +1,29 @@
 import { Button, Col, Container, Row, Card } from "react-bootstrap";
+import { useContext } from "react";
+import { MyContext } from "../store/Store";
+import { useParams, useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
+import { API } from "../config/api";
 
 const Detail = () => {
+  const { loginState } = useContext(MyContext);
+
+  // mengambil id trip dari url
+  const id = useParams().id;
+  const navigate = useNavigate();
+
+  const { data: detailProduct, isLoading: detailProductIsLoading } = useQuery(
+    "detailProductCache",
+    async () => {
+      try {
+        const response = await API.get(`/product/${id}`);
+        return response.data.data;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  );
+
   return (
     <main style={{ marginTop: 150 }}>
       <Container>
@@ -13,7 +36,7 @@ const Detail = () => {
               lg={5}
               className="d-flex justify-content-center align-items-center"
             >
-              <Card.Img src="/assets/product.svg" alt="Product" fluid />
+              <Card.Img src={detailProduct?.image} alt="Product" fluid />
             </Col>
             <Col
               lg={7}
@@ -24,39 +47,20 @@ const Detail = () => {
                   className="display-4 fw-bold"
                   style={{ color: "#613D2B" }}
                 >
-                  GUETAMALA Beans
+                  {detailProduct?.name}
                 </Card.Title>
                 <Card.Subtitle style={{ color: "#9D5453" }}>
-                  Stock : 500
+                  Stock : {detailProduct?.stock}
                 </Card.Subtitle>
 
                 <Card.Text className="my-3" style={{ textAlign: "justify" }}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-                  vero eum cum ducimus optio ut perspiciatis, nisi nemo
-                  voluptatum deserunt odio, dicta reprehenderit, et omnis
-                  nostrum sed deleniti. Voluptate rerum blanditiis eveniet
-                  doloribus sapiente quibusdam voluptas quidem, sunt ut dolor
-                  odit? Reiciendis dolores sunt laborum ex ipsa reprehenderit,
-                  unde dignissimos asperiores ipsam eos facilis et quibusdam,
-                  accusantium nihil! Animi vitae qui ducimus deserunt! Facere
-                  voluptates eaque, sunt placeat reprehenderit eum, eligendi
-                  numquam consectetur dolores quod, odit libero modi ipsam
-                  soluta omnis quia veniam beatae odio. Nostrum temporibus
-                  consequuntur nisi consectetur vel voluptatibus qui placeat
-                  odio natus repudiandae ratione facere ea animi dolorem
-                  reiciendis corrupti ad excepturi, a magni pariatur omnis esse
-                  nam voluptatum neque. Inventore amet tempora ducimus magni
-                  harum ea iusto laborum, repudiandae adipisci, voluptate
-                  tenetur saepe. Distinctio debitis, quibusdam maiores quasi
-                  corrupti laboriosam ab praesentium nam eligendi inventore
-                  dicta perferendis deleniti sequi molestias, nobis ut.
-                  Suscipit, harum dolorum!
+                  {detailProduct?.description}
                 </Card.Text>
                 <Card.Subtitle
                   style={{ color: "#9D5453" }}
                   className="text-end my-5 fs-2 fw-bold"
                 >
-                  Rp 300.900,-
+                  Rp {detailProduct?.price.toLocaleString()},-
                 </Card.Subtitle>
 
                 <Button
