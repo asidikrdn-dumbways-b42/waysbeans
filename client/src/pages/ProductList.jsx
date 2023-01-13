2db504a902328cce3a3919997b1913868b453eca
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-01-13 08:52:24 +0700

    fetch product & handle delete product in list product page

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,6 +1,32 @@
-import { Button, Container, Table } from "react-bootstrap";
+import { Button, Container, Table, Image } from "react-bootstrap";
+import { useQuery, useMutation } from "react-query";
+import { API } from "../config/api";
 
 const ProductList = () => {
+  const {
+    data: productData,
+    isisLoading: productDataIsLoading,
+    refetch: productDataRefetch,
+  } = useQuery("productDataCache", async (e) => {
+    try {
+      const response = await API.get("/products");
+      return response.data.data;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const handleDeleteProduct = useMutation(async (id) => {
+    try {
+      const response = await API.delete(`/product/${id}`);
+      if (response.data.status === "success") {
+        productDataRefetch();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
   return (
     <main style={{ marginTop: 150 }}>
       <Container fluid style={{ padding: "0 150px" }}>
@@ -10,10 +36,15 @@ const ProductList = () => {
         <Table
           bordered
           className="mt-5"
-          style={{ border: "1px solid #8E8E8E" }}
+          // style={{ border: "1px solid #8E8E8E" }}
         >
           <thead>
-            <tr style={{ backgroundColor: "#E5E5E5" }}>
+            <tr
+              style={{
+                backgroundColor: "#E5E5E5",
+                border: "1px solid #8E8E8E",
+              }}
+            >
               <th className="py-3 text-start">No</th>
               <th className="py-3 text-center">Image</th>
               <th className="py-3 text-center">Name</th>
@@ -32,54 +63,44 @@ const ProductList = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td className="text-center">Lihat Gambar</td>
-              <td className="text-center">RWANDA Beans</td>
-              <td className={"text-center"}>150</td>
-              <td className={"text-center"}>Rp 60.900,-</td>
-              <td className={"text-center"}>Ini deskripsi.....</td>
-              <td className="text-center">
-                <Button variant="danger" className="py-1 px-4 mx-2">
-                  Delete
-                </Button>
-                <Button variant="success" className="py-1 px-4 mx-2">
-                  Update
-                </Button>
-              </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td className="text-center">Lihat Gambar</td>
-              <td className="text-center">RWANDA Beans</td>
-              <td className={"text-center"}>150</td>
-              <td className={"text-center"}>Rp 60.900,-</td>
-              <td className={"text-center"}>Ini deskripsi.....</td>
-              <td className="text-center">
-                <Button variant="danger" className="py-1 px-4 mx-2">
-                  Delete
-                </Button>
-                <Button variant="success" className="py-1 px-4 mx-2">
-                  Update
-                </Button>
-              </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td className="text-center">Lihat Gambar</td>
-              <td className="text-center">RWANDA Beans</td>
-              <td className={"text-center"}>150</td>
-              <td className={"text-center"}>Rp 60.900,-</td>
-              <td className={"text-center"}>Ini deskripsi.....</td>
-              <td className="text-center">
-                <Button variant="danger" className="py-1 px-4 mx-2">
-                  Delete
-                </Button>
-                <Button variant="success" className="py-1 px-4 mx-2">
-                  Update
-                </Button>
-              </td>
-            </tr>
+            {productData?.map((product, i) => {
+              return (
+                <tr>
+                  <td>{i + 1}</td>
+                  <td className="text-center" valign="middle">
+                    <Image src={product.image} alt={product.name} />
+                  </td>
+                  <td className="text-center" valign="middle">
+                    <h3>{product.name}</h3>
+                  </td>
+                  <td className={"text-center"} valign="middle">
+                    <h5>{product.stock}</h5>
+                  </td>
+                  <td className={"text-center"} valign="middle">
+                    <h5>Rp {product.price.toLocaleString()},-</h5>
+                  </td>
+                  <td className={"text-center"} valign="middle">
+                    <p style={{ textAlign: "justify" }}>
+                      {product.description}
+                    </p>
+                  </td>
+                  <td className="text-center" valign="middle">
+                    <Button
+                      variant="danger"
+                      className="py-1 px-4 mx-2"
+                      onClick={() => {
+                        handleDeleteProduct.mutate(product.id);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                    <Button variant="success" className="py-1 px-4 mx-2">
+                      Update
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </Table>
       </Container>
