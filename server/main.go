29f58b3cf6251a59663be01d855b080d11f86583
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-01-15 20:12:54 +0700

    refactor main.go

--- server/main.go
+++ server/main.go
@@ -49,5 +49,5 @@ func main() {
 
 	// menjalankan web server
 	fmt.Println("server running localhost:" + os.Getenv("PORT"))
-	http.ListenAndServe("localhost:"+os.Getenv("PORT"), handlers.CORS(AllowedHeaders, AllowedOrigins, AllowedMethods)(r))
+	http.ListenAndServe(":"+os.Getenv("PORT"), handlers.CORS(AllowedHeaders, AllowedOrigins, AllowedMethods)(r))
 }
